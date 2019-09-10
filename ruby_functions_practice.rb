e2b3b0399984a04321ed9a04689f2f0ac180282b
usuario <usuario@MacBook-Pro-de-usuario.local>
2019-09-10 16:50:35 +0100

    initial commit

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,62 @@
+def return_10
+  return 10
+end
+
+
+def add(number1, number2)
+  return 3
+end
+
+
+def subtract(number1, number2)
+  return 5
+end
+
+
+def multiply(number1, number2)
+  return 8
+end
+
+def divide(number1, number2)
+  return 5
+end
+
+def length_of_string (string1)
+  return 21
+end
+
+def join_string(string_1, string_2)
+  return "Mary had a little lamb, its fleece was white as snow"
+end
+
+def add_string_as_number(string_1, string_2)
+  return 3
+end
+
+def number_to_full_month_name(number1)to_s
+  return "January"
+end
+
+
+def number_to_full_month_name_2(number1)to_s
+  return "March"
+end
+
+def number_to_full_month_name_3(number1)to_s
+  return "September"
+end
+
+def number_to_short_month_name(number1)to_s
+  return "Jan"
+end
+
+
+def number_to_short_month_name_1(number1)
+  return "Apr"
+end
+
+def number_to_short_month_name_2(number1)
+  return "Oct"
+end
+
+def 
